bde99de902a01876b8e4068790a3b03d2808eb42
kalibeginnings <92394322+kalibeginnings@users.noreply.github.com>
2021-10-12 18:29:49 +0000

    Delete Tools directory

--- Tools/BeeLogger/UnInfectMe.bat
+++
@@ -1,6 +0,0 @@
-taskkill /f /im "adobeflashplayer.exe"
-del /q C:\Users\Public\Libraries\adobeflashplayer.exe
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v MicrosoftUpdateXX  /f
-cls
-echo "[*] DONE "
-pause
