6fcc084261306895599f7519ee6626f75f2c4dc7
kalibeginnings <92394322+kalibeginnings@users.noreply.github.com>
2023-02-11 01:20:54 -0500

    saved

--- Kali/setup.sh
+++ Kali/setup.sh
@@ -4,11 +4,11 @@ mv $HOME/.zshrc /tmp
 mv /root/.zshrc /tmp
 mv /etc/proxychains4.conf /tmp
 mv /etc/ssh/sshd_config /tmp
-ln -s $HOME/Sync/Kali/Configs/zshrc $HOME/.zshrc
-ln -s $HOME/Sync/Kali/Configs/zshrc /root/.zshrc
-ln -s $HOME/Sync/Kali/Configs/proxychains4.conf /etc/proxychains4.conf
-ln -s $HOME/Sync/Kali/Configs/sshd_config /etc/ssh/sshd_config
-ln -s $HOME/Sync/Kali/Configs/zsh_aliases $HOME/.zsh_aliases
-ln -s $HOME/Sync/Kali/Configs/zsh_ngrok $HOME/.zsh_ngrok
-ln -s $(find $HOME/Sync -maxdepth 1 -type d) $HOME/
-ln -s $(find $HOME/Sync -maxdepth 1 -type d) /root/
+ln -sf $HOME/Sync/Kali/Configs/zshrc $HOME/.zshrc
+ln -sf $HOME/Sync/Kali/Configs/zshrc /root/.zshrc
+ln -sf $HOME/Sync/Kali/Configs/proxychains4.conf /etc/proxychains4.conf
+ln -sf $HOME/Sync/Kali/Configs/sshd_config /etc/ssh/sshd_config
+ln -sf $HOME/Sync/Kali/Configs/zsh_aliases $HOME/.zsh_aliases
+ln -sf $HOME/Sync/Kali/Configs/zsh_ngrok $HOME/.zsh_ngrok
+ln -sf $(find $HOME/Sync -maxdepth 1 -type d) $HOME/
+ln -sf $(find $HOME/Sync -maxdepth 1 -type d) /root/
